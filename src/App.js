0eae67719949518d42e908dec21a09dfd49bff6d
IHECHI <nnatahi57@gmail.com>
2022-05-22 19:33:44 +0100

    updated myAPP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import TodoForm from './component /TodoForm'
 import TodoList from './component /TodoList'
 function App() {
   return (
     <div className="todo-app">
-   
      <TodoList/>
     </div>
   )
