2f8b2024a787769d950502e52a8d10bdb51a204e
IHECHI <nnatahi57@gmail.com>
2022-05-23 16:21:30 +0100

    new update

--- src/component /Todo.js
+++ src/component /Todo.js
@@ -1,62 +1,10 @@
-// import React, { useState } from 'react'
-// import { RiCloseCircleLine } from 'react-icons/ri'
-// import { TiEdit } from 'react-icons/ti'
-// import TodoForm from './TodoForm'
-// function Todo({ todos, completeTodo, removeTodo, updatedTodo  }) {
-//     const [edit, setEdit] = useState({
-//         id: null,
-//         value: ''
-//     });
-//     const submitUpdate = value => { 
-//         updatedTodo(edit.id, value)
-//         setEdit({
-//             id: null,
-//             value: ""
-//         })
-//         if (edit.id ){
-//             return <TodoForm edit={edit} onSubmit={submitUpdate} />;
-            
-//         }
-//     }
-
-//     return (
-//         <div>
-//             {
-//                 todos.map((todo, index) =>
-//                 (<div
-//                     className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
-//                     key={index}
-
-//                 >
-//                     <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-//                         {todo.text}
-//                     </div>
-//                     <div className="icons">
-//                         <RiCloseCircleLine
-//                             onClick={() => removeTodo(todo.id)}
-//                             className="delete-icon" />
-
-//                         <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })}
-//                             className="edit-icon" />
-//                     </div>
-//                 </div>)
-//                 )
-//             }
-//         </div>
-//     )
-
-
-// }
-
-// export default Todo
-
-   
 import React, { useState } from 'react';
 import TodoForm from './TodoForm';
 import { RiCloseCircleLine } from 'react-icons/ri';
 import { TiEdit } from 'react-icons/ti';
-
-const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+import {FiCheckCircle} from 'react-icons/fi'
+const Todo = ({ todo, completeTodo, removeTodo, updateTodo, doneTodo}) => {
+  // console.log(todos);
   const [edit, setEdit] = useState({
     id: null,
     value: ''
@@ -74,26 +22,54 @@ const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
     return <TodoForm edit={edit} onSubmit={submitUpdate} />;
   }
 
-  return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
-      key={index}
-    >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-      <div className='icons'>
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className='delete-icon'
-        />
+  return (
+    <>
+      {/* <div
+        className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+        key={index}
+      >
+        <div key={todo.id}>
+          {todo.text}
+        </div>
+  
+  
+        <div className='icons'>
+          <RiCloseCircleLine
+            className='delete-icon'
+          />
+          <TiEdit
+            onClick={() => setEdit({ id: todo.id, value: todo.text })}
+            className='edit-icon'
+          />
+          <FiCheckCircle
+            onClick={()=>doneTodo(todo.id)}
+            className='done-icon'
+          />
+  
+        </div>
+      </div> */}
+      <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}>
+          <p>{todo.text}</p>
+
+          <div>
+            <RiCloseCircleLine
+              className='delete-icon'
+              onClick={()=>removeTodo(todo.id)}
+            />
+
         <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className='edit-icon'
-        />
+            onClick={() => setEdit({ id: todo.id, value: todo.text })}
+            className='edit-icon'
+          />
+
+            <FiCheckCircle
+              onClick={()=>{doneTodo(todo.id)}}
+              className='done-icon'
+            />
+          </div>
       </div>
-    </div>
-  ));
+    </>
+  )
 };
 
 export default Todo;
